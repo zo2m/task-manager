c14a797844753f0ae4e2f44c5b3520276ef40b24
Alexandre <alexandre@zo2m.com.br>
2015-08-20 20:49:27 -0300

    Correção de parâmetro
    Alteração do parâmetro owner_id para user_id

--- app/Validators/ProjectValidator.php
+++ app/Validators/ProjectValidator.php
@@ -14,7 +14,7 @@ use Prettus\Validator\LaravelValidator;
 class ProjectValidator extends LaravelValidator
 {
     protected $rules = [
-        'owner_id' => 'required|integer',
+        'user_id' => 'required|integer',
         'client_id'=> 'required|integer',
         'name' => 'required',
         'progress' => 'required',
